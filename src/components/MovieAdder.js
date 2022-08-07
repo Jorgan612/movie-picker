ecd22c506aa841f77c3afde06b1812947c11a53d
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-08-06 19:45:49 -0600

    Work on functionality for updating movies array state. Issue with infinite loop currently.

--- src/components/MovieAdder.js
+++ src/components/MovieAdder.js
@@ -1,10 +1,24 @@
-const MovieAdder = () => {
+import { useState } from 'react';
+
+const MovieAdder = ({addMovie}) => {
+
+  const [newMovie, setNewMovie] = useState('')
+
+  // const clearInput = () => {
+  //   setNewMovie('')
+  // }
+
   return (
     <section className="movie-adder-section">
       <h4>Start by adding movies you like, love, or haven't seen yet!</h4>
       <div className="add-movie-input-div">
-        <input type='text' name='title' placeholder='Movie Title' required/>
-        <button className="add-movie-btn">+</button>
+        <input 
+          type='text' 
+          name='title' 
+          placeholder='Movie Title' 
+          onChange={(event) => setNewMovie(event.target.value)} 
+        />
+        <button className="add-movie-btn" onClick={addMovie(newMovie)}>+</button>
       </div>
     </section>
   )
---
+++ src/components/MovieContainer.js
@@ -0,0 +1,16 @@
+import '../css/App.css';
+
+const MovieContainer = ({movies}) => {
+  
+  // const getRandomMovie = () => {
+    
+  // }
+
+ return (
+  <div className='movie-container-div'>
+    <button className='random-movie-btn'>Get Movie!</button>
+  </div>
+ )
+}
+
+export default MovieContainer;
