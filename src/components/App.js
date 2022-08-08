f99934a7a6bebb60fea8cae07139c32670d34d96
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-08-08 09:58:33 -0600

    Reorganize methods in App and movieAdder component. Move state to MovieAdder component.

--- src/components/App.js
+++ src/components/App.js
@@ -1,22 +1,13 @@
 import '../css/App.css';
 import MovieAdder from '../components/MovieAdder';
-import MovieContainer from '../components/MovieContainer';
-import { useState } from 'react';
 
 const App = () => {
-  const [movies, setMovies] = useState([]);
-  
 
-  const addMovie = (newMovie) => {
-    // setMovies([...movies, newMovie]);
-    console.log('movies array', movies )
-  }
 
   return (
     <main>
       <h1>Not sure what to watch?</h1>
-      <MovieAdder addMovie={addMovie}/>
-      {movies.length > 1 ? <MovieContainer movies={movies} /> : <p>Waiting on movies to be added...</p>}
+      <MovieAdder />
     </main>
   )
 }
--- src/components/MovieAdder.js
+++ src/components/MovieAdder.js
@@ -1,12 +1,25 @@
 import { useState } from 'react';
+import MovieContainer from '../components/MovieContainer';
 
-const MovieAdder = ({addMovie}) => {
+
+const MovieAdder = () => {
 
   const [newMovie, setNewMovie] = useState('')
+  const [movies, setMovies] = useState([]);
 
-  // const clearInput = () => {
-  //   setNewMovie('')
-  // }
+   const addMovie = () => {
+    setMovies([...movies, newMovie]); 
+    clearInput(); 
+    console.log('movies array', movies )
+  }
+
+  const clearInput = () => {
+    setNewMovie('')
+  }
+
+  const determineMovie = () => {
+    return Math.floor(Math.random() * movies.length);
+  }
 
   return (
     <section className="movie-adder-section">
@@ -16,10 +29,12 @@ const MovieAdder = ({addMovie}) => {
           type='text' 
           name='title' 
           placeholder='Movie Title' 
+          value={newMovie}
           onChange={(event) => setNewMovie(event.target.value)} 
         />
-        <button className="add-movie-btn" onClick={addMovie(newMovie)}>+</button>
+        <button className="add-movie-btn" onClick={addMovie}>+</button>
       </div>
+      {movies.length > 1 ? <MovieContainer movies={movies} /> : <p>Waiting on movies to be added...</p>}
     </section>
   )
 }
