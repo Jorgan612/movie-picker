560b2547c1d676d88b90ed125ae2042e84065940
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-08-13 19:34:48 -0600

    Use BrowserRouter in index.js file.

--- src/components/MovieAdder.js
+++ src/components/MovieAdder.js
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react';
+import { Routes, Route } from 'react-router-dom';
 import MovieContainer from '../components/MovieContainer';
 
 const MovieAdder = () => {
