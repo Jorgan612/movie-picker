12fcb17bc13c213d4f0df60126c0bdc58f735670
Jessica Organ <83175748+Jorgan612@users.noreply.github.com>
2022-08-16 18:24:22 -0600

    Begin functionality for movie list search feature.

--- src/components/MovieList.js
+++ src/components/MovieList.js
@@ -1,4 +1,7 @@
+import { useState } from  'react';
+
 const MovieList = ({movies, removeMovie}) => {
+  const [searchPhrase, setSearchPhrase] = useState();
   const listMovies = movies.map((movie) => {
     return <article key ={movie} id={movie} className="movie-article-card">
       <div className="movie-card-top">
@@ -8,10 +11,20 @@ const MovieList = ({movies, removeMovie}) => {
     </article>
   })
 
+  const findMovie = () => {
+    
+  }
+
   return (
     <section className="movie-list-container">
       <div className="movie-list-container-top">
         <h1 className="movie-list-component-title">Movie List</h1>
+        <input 
+          name='search' 
+          placeholder="Search for Title..." 
+          type='text' 
+          aria-label='move title'
+          onChange={findMovie()}/>
       </div>
       <div className="movie-list-div">
         {listMovies}
